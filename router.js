746530658563957159c96746fbcf8f9de5de8643
Guilherme Rizzo <guivr2011@gmail.com>
2016-10-20 19:05:48 -0200

    ES6

--- router.js
+++ router.js
@@ -1,12 +1,14 @@
 'use strict'
 
-var Router = {
-    actualRoute: '',
-    reset: function () {
+const Router = {
+    currentRoute: '',
+
+    reset() {
       this.routes = [];
     },
-    loadRoute: function (name) {
-        var xmlhttp = new XMLHttpRequest();
+
+    loadRoute(name) {
+        let xmlhttp = new XMLHttpRequest();
 
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
@@ -25,37 +27,39 @@ var Router = {
         xmlhttp.open('GET', name+'.html', true);
         xmlhttp.send();
     },
-    getActualRoute: function () {
-        this.actualRoute = location.hash == '#/' ? '':location.hash.split('#/')[1];
 
-        var checkRoute = this.routes;
+    getCurrentRoute() {
+        this.currentRoute = location.hash == '#/' ? '':location.hash.split('#/')[1];
 
-        for (var i = 0; i <= checkRoute.length-1; i++){
-            if(typeof checkRoute[i].route === 'object'){
+        const routes = this.routes;
+
+        for (let i = 0; i <= routes.length-1; i++){
+            if(typeof routes[i].route === 'object'){
                 if(location.hash.split('#/')[1]){
-                    for (var j = 0; j <= checkRoute[i].route.length-1; j++) {
-                        if(location.hash.split('#/')[1] == checkRoute[i].route[j]) {
-                            this.actualRoute = checkRoute[i].route[j];
+                    for (let j = 0; j <= routes[i].route.length-1; j++) {
+                        if(location.hash.split('#/')[1] == routes[i].route[j]) {
+                            this.currentRoute = routes[i].route[j];
                         }
                     }
                 } else {
-                    this.actualRoute = '';
+                    this.currentRoute = '';
                 }
             } else {
-                if(location.hash.split('#/')[1] == checkRoute[i].route){
-                    this.actualRoute = checkRoute[i].route;
+                if(location.hash.split('#/')[1] == routes[i].route){
+                    this.currentRoute = routes[i].route;
                 }
             }
         }
 
-    	this.navigateToRoute(this.actualRoute);
+    	this.navigateToRoute(this.currentRoute);
     },
-    configMap: function (routes) {
-    	this.routes = routes;
-    	this.getActualRoute();
 
-        var nav_html = '';
-        for (var i = 0; i <= routes.length-1; i++) {
+    configMap(routes) {
+    	this.routes = routes;
+        this.getCurrentRoute();
+
+        let nav_html = '';
+        for (let i = 0; i <= routes.length-1; i++) {
             if(routes[i].nav) {
                 if(typeof routes[i].route === 'object'){
                     nav_html += '<a href="#/'+routes[i].route[0]+'" class="route-changer"><li>'+routes[i].title+'</li></a>';
@@ -64,24 +68,25 @@ var Router = {
                 }
             }
         }
-        var nav = document.getElementsByTagName('nav')[0].innerHTML = '<ul>'+nav_html+'</ul>';
+        const nav = document.getElementsByTagName('nav')[0].innerHTML = '<ul>'+nav_html+'</ul>';
 
         getRouteChangers();
     },
-    navigateToRoute: function (route) {
-    	this.actualRoute = route;
-    	location.hash = '/'+this.actualRoute;
 
-        for (var i = 0; i <= this.routes.length-1; i++){
+    navigateToRoute(route) {
+    	this.currentRoute = route;
+    	location.hash = '/'+this.currentRoute;
+
+        for (let i = 0; i <= this.routes.length-1; i++){
             if(typeof this.routes[i].route === 'object'){
-                for (var j = 0; j <= this.routes[i].route.length-1; j++) {
-                    if(this.routes[i].route[j] == this.actualRoute){
+                for (let j = 0; j <= this.routes[i].route.length-1; j++) {
+                    if(this.routes[i].route[j] == this.currentRoute){
                         this.loadRoute(this.routes[i].moduleId);
                         document.title = this.routes[i].title;
                     }
                 }
             } else {
-                if(this.actualRoute == this.routes[i].route){
+                if(this.currentRoute == this.routes[i].route){
                     this.loadRoute(this.routes[i].moduleId);
                     document.title = this.routes[i].title;
                 }
@@ -91,8 +96,8 @@ var Router = {
 };
 
 function getRouteChangers() {
-    var routeChangers = document.querySelectorAll(".route-changer");
-    for (var i = 0; i <= routeChangers.length-1; i++) {
+    let routeChangers = document.querySelectorAll(".route-changer");
+    for (let i = 0; i <= routeChangers.length-1; i++) {
         if (document.addEventListener) {
             routeChangers[i].addEventListener('click', function() {
                 Router.navigateToRoute(this.getAttribute('href').split('#/')[1]);
